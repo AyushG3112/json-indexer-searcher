94e0052050bba7be224705a4ef73449ae7a1c6ae
Ayush Gupta <ayushg3112@gmail.com>
2017-07-09 18:36:40 +0530

    indexByMultipleFields POC

--- dist/indexer.js
+++ dist/indexer.js
@@ -17,6 +17,26 @@ var Indexer = (function () {
         }
         return resultData;
     };
+    Indexer.prototype.indexByMultipleFields = function (arrayToIndex, keysToIndexBy) {
+        var resultData = {
+            fieldsIndexedBy: keysToIndexBy.slice(),
+        };
+        for (var i in arrayToIndex) {
+            var valuesToIndexBy = [""];
+            for (var j in keysToIndexBy) {
+                var value = arrayToIndex[i][keysToIndexBy[j]];
+                if (!typeChecker.isString(value) && !typeChecker.isNumber(value)) {
+                    throw new Error('Can only index by array or number. Provided: ' + typeChecker.getType(value));
+                }
+                valuesToIndexBy = valuesToIndexBy.concat([value]);
+            }
+            valuesToIndexBy.shift();
+            resultData[valuesToIndexBy.join(', ')] = arrayToIndex[i];
+        }
+        return resultData;
+    };
     return Indexer;
 }());
 module.exports = new Indexer();
+var test = new Indexer();
+console.log(test.indexByMultipleFields(require('../city.list.json'), ["name", "country"]));
--- src/indexer.ts
+++ src/indexer.ts
@@ -14,6 +14,26 @@ class Indexer {
         }
         return resultData;
     }
+
+    indexByMultipleFields(arrayToIndex: Array<any>, keysToIndexBy: Array<string>) : Object {
+        let resultData = {
+            fieldsIndexedBy : [...keysToIndexBy],
+        }
+        for(let i in arrayToIndex) {
+            let valuesToIndexBy = [""];
+            for(let j in keysToIndexBy) {
+                let value = arrayToIndex[i][keysToIndexBy[j]];
+                if(!typeChecker.isString(value) && !typeChecker.isNumber(value)) {
+                    throw new Error('Can only index by array or number. Provided: '+typeChecker.getType(value));
+                }
+                valuesToIndexBy = [...valuesToIndexBy, value]
+            }
+
+            valuesToIndexBy.shift();
+            resultData[valuesToIndexBy.join(', ')] = arrayToIndex[i];
+        }
+        return resultData;
+    }
 }
 
 module.exports = new Indexer();
